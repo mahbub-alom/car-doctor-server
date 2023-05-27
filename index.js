248c5a7c9e52a46f011a1bf5f91a2b0c3c9b7aeb
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-05-27 18:45:43 +0600

    asending and desending added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
@@ -21,20 +21,24 @@ const client = new MongoClient(uri, {
   },
 });
 
-const verifyJWT =(req,res,next)=>{
+const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error:true,message:'unauthorized access'})
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
-  const token = authorization.split(' ')[1];
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
-    if(error){
-      return res.status(403).send({error:true,message:'unauthorized access'})
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
     }
-    req.decoded=decoded;
+    req.decoded = decoded;
     next();
-  })
-}
+  });
+};
 
 async function run() {
   try {
@@ -44,20 +48,28 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("services");
     const checkOutCollection = client.db("carDoctor").collection("bookings");
 
-        // jwt
-        app.post('/jwt', (req, res) => {
-          const user = req.body;
-          console.log(user);
-          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-          console.log(token);
-          res.send({token});
-      })
-
-
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
 
     //services related
     app.get("/services", async (req, res) => {
-      const cursor = serviceCollection.find();
+      const sort = req.query.sort;
+      const query = {price:{$lte:150}};
+      const options = {
+        // sort matched documents in descending order by rating
+        sort: {
+          price: sort === "asc" ? 1 : -1
+        }
+      };
+      const cursor = serviceCollection.find(query, options);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -73,14 +85,13 @@ async function run() {
     });
 
     //bookings routes
-    app.get("/checkout",verifyJWT, async (req, res) => {
-      const decoded=req.decoded;
-      console.log('came back after verify', decoded);
+    app.get("/checkout", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log("came back after verify", decoded);
 
-      if(decoded.email !== req.query.email){
-        return res.status(402).send({error:1,message:'forbidden access'})
+      if (decoded.email !== req.query.email) {
+        return res.status(402).send({ error: 1, message: "forbidden access" });
       }
-
 
       let query = {};
       if (req.query?.email) {
@@ -96,23 +107,23 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/checkout/:id',async(req,res)=>{
+    app.patch("/checkout/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id:new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
       console.log(updatedBooking);
       const updateDoc = {
-        $set:{
-          status:updatedBooking.status
-        }
-      }
-      const result = await checkOutCollection.updateOne(filter,updateDoc);
-      res.send(result)
-    })
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+      const result = await checkOutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     app.delete("/checkout/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await checkOutCollection.deleteOne(query);
       res.send(result);
     });
